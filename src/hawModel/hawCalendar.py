1a6f2a20d0aab50e2dc74a5087c3012546ed1b8f
Theodor Nolte <theno@gmx.net>
2013-09-13 19:57:29 +0200

    Formattings

--- src/hawModel/hawCalendar.py
+++ src/hawModel/hawCalendar.py
@@ -22,7 +22,7 @@ import logging
 
 from icalendar import Icalendar
 
-# named indices for eventTupel ("tupelKeyIndices")
+# named indices for eventTupel (used as "tupelKeyIndex")
 SEMESTERGRUPPE, GRUPPENKUERZEL, FACH, DOZENT, RAUM, JAHR, WOCHE, WOCHENTAG, ANFANG, ENDE, INFOSTRING = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 
 class HawCalendar:
